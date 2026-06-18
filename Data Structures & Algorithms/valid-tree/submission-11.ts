8526cd611ea3e76f9a9d6dc691ed3cf863ff671d
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-06-18 14:31:18 +0530

    Add: valid-tree - submission-11

---
+++ Data Structures & Algorithms/valid-tree/submission-11.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n: number, edges: number[][]): boolean {
+        if(edges.length!==n-1) return false
+        const adj = Array.from({length:n},()=>[])
+        for(const [u,v] of edges) {
+            adj[u].push(v)
+            adj[v].push(u)
+        }
+
+        const visit = new Set()
+        function dfs(node, parent) {
+            if(visit.has(node)) return false
+            visit.add(node)
+            for(const nei of adj[node]) {
+                if(nei===parent) continue
+                if(!dfs(nei, node)) {
+                    return false
+                }
+            }
+            return true
+        }
+        return dfs(0, -1) && visit.size===n
+    }
+}
