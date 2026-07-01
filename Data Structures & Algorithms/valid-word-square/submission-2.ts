eac28c90e5e51db7c694d9bbbf08d580c7d1df7c
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-07-01 21:49:46 +0530

    Add: valid-word-square - submission-2

---
+++ Data Structures & Algorithms/valid-word-square/submission-2.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {boolean}
+     */
+    validWordSquare(words: string[]): boolean {
+        let cols = 0
+        let rows = words.length
+        let newWords = []
+
+        for(let word of words) {
+            cols = Math.max(cols, word.length)
+        }
+
+        if(cols!==rows) return false
+
+        for(let col = 0; col<cols; col++) {
+            let newWord = ''
+            for(let row = 0; row<rows; row++) {
+                if(col<words[row].length) {
+                    newWord += words[row][col]
+                }
+            }
+            newWords.push(newWord)
+        }
+        return words.every((value, i)=> {
+            return value === newWords[i]
+        })
+    }
+}
