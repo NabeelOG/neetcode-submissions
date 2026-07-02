ffea9dcc884989ec3a2dc7ef3e2746c1a2aeffde
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-07-02 16:21:51 +0530

    Add: single-row-keyboard - submission-0

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} keyboard
+     * @param {string} word
+     * @return {number}
+     */
+    calculateTime(keyboard: string, word: string): number {
+        const keyIndices = {}
+
+        for(let i = 0; i<keyboard.length; i++)
+            keyIndices[keyboard[i]] = i
+
+        let prev = 0;
+        let result = 0;
+
+        for(const c of word) {
+            result += Math.abs(prev-keyIndices[c]);
+            prev = keyIndices[c];
+        }
+        return result;
+    }
+}
