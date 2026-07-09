c0f8c979c72473e98cff4891172c8a2e6c61e4ef
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-07-09 17:52:31 +0530

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    stringMatching(words: string[]): string[] {
+        let res = []
+        words.sort((a,b)=>a.length-b.length)
+
+        for(let i = 0; i<words.length; i++) {
+            for(let j = i+1; j<words.length; j++) {
+                if(words[j].includes(words[i])) {
+                    res.push(words[i])
+                    break
+                }
+            }
+        }
+        return res
+    }
+}
