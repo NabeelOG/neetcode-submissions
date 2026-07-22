3f04c1ececa2c739eeae3ed6e2dc58383118a6b8
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-07-22 14:28:29 +0530

    Add: number-of-distinct-islands - submission-2

---
+++ Data Structures & Algorithms/number-of-distinct-islands/submission-2.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    numDistinctIslands(grid: number[][]): number {
+        const ROWS = grid.length
+        const COLS = grid[0].length
+        const uniqueIsland = new Set<string>();
+
+        function dfs(r:number, c:number, baseR:number, baseC:number, shape: string[]): void {
+            grid[r][c] = 0
+
+            const relativeR = r-baseR;
+            const relativeC = c-baseC;
+            shape.push(`${relativeR},${relativeC}`);
+
+            const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            for(const [dr, dc] of directions) {
+                const nr = r+dr;
+                const nc = c+dc;
+
+                if(nr>=0 && nr<ROWS && nc>=0 && nc<COLS && grid[nr][nc]===1) {
+                    dfs(nr, nc, baseR, baseC, shape);
+                }
+            }
+        }
+
+        for(let r = 0; r<ROWS; r++) {
+            for(let c = 0; c<COLS; c++) {
+                if(grid[r][c]===1) {
+                    const shape: string[]=[]
+                    dfs(r,c,r,c,shape)
+
+                    uniqueIsland.add(shape.join(':'));
+                }
+            }
+        }
+
+        return uniqueIsland.size;
+    }
+}
