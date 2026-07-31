371ae56f4c860c132dfddd5d4b145d71960b2e20
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-07-31 15:39:42 +0530

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestMonotonicSubarray(nums: number[]): number {
+        let inc = 1, dec = 1, res = 1
+        for(let i = 1; i<nums.length; i++) {
+            if(nums[i]===nums[i-1]) {
+                inc = dec = 1
+            } else if(nums[i]>nums[i-1]) {
+                inc++
+                dec = 1
+            } else {
+                dec++
+                inc = 1
+            }
+            res = Math.max(res, inc, dec)
+        }
+        return res
+    }
+}
