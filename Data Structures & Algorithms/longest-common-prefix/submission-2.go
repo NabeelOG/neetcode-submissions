73962b532272f0955b406c2d12869fb509e6569f
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-08-19 22:12:15 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.go
@@ -0,0 +1,13 @@
+func longestCommonPrefix(strs []string) string {
+    sort.Strings(strs)
+    first := strs[0]
+    last := strs[len(strs)-1]
+    res := ""
+    for i := 0; i < len(first) && i < len(last); i++ {
+        if first[i] != last[i] {
+            return res
+        }
+        res += string(first[i])
+    }
+    return res
+}
