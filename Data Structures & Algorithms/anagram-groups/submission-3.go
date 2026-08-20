766ca4f3dd1b53d00eb2575529aecca5daaa688a
Nabeel Muhammed <98878048+NabeelOG@users.noreply.github.com>
2026-08-20 17:21:20 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.go
@@ -0,0 +1,16 @@
+import "slices"
+func groupAnagrams(strs []string) [][]string {
+	groups := make(map[string][]string, len(strs))
+
+	for _, s := range strs {
+		key := []byte(s)
+		slices.Sort(key)
+		groups[string(key)] = append(groups[string(key)], s)
+	}
+
+	res := make([][]string, 0, len(groups))
+	for _, group := range groups {
+		res = append(res, group)
+	}
+	return res
+}
